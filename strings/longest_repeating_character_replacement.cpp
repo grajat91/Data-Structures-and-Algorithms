1f423994f74b842f3be9d06b78d33787dd528977
grajat91 <72464777+grajat91@users.noreply.github.com>
2022-05-31 17:25:47 -0700

    Create longest_repeating_character_replacement.cpp

---
+++ strings/longest_repeating_character_replacement.cpp
@@ -0,0 +1,35 @@
+/*You are given a string s and an integer k. You can choose any character of the string and change it to any other uppercase English character. You can perform this operation at most k times.
+
+Return the length of the longest substring containing the same letter you can get after performing the above operations.
+
+Example 1:
+Input: s = "ABAB", k = 2
+Output: 4
+Explanation: Replace the two 'A's with two 'B's or vice versa.
+
+Example 2:
+Input: s = "AABABBA", k = 1
+Output: 4
+Explanation: Replace the one 'A' in the middle with 'B' and form "AABBBBA".
+The substring "BBBB" has the longest repeating letters, which is 4.*/
+
+    int characterReplacement(string s, int k) {
+        int count[26] = {0};
+        int stringlen = s.length();
+        int l = 0;
+        int max_occ = 0;;
+        for(int r=0; r < stringlen; r++) {
+            count[s[r]-'A']++;
+            //int window_length = r-l+1;
+            int window_max = 0;
+            for (int i=0;i<26;i++) {
+                window_max = max(window_max,count[i]);
+            }
+            if ((r-l+1) - window_max > k) {
+                    count[s[l]-'A']--;
+                    l++;
+            }
+            max_occ = max(max_occ,(r-l+1));
+        }
+        return max_occ;
+    }
