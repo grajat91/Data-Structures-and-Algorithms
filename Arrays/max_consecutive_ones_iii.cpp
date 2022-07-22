110964ad4bbd5f19802bc2094483a4ab3639bb65
grajat91 <72464777+grajat91@users.noreply.github.com>
2022-07-22 13:30:52 -0700

    Update max_consecutive_ones_iii.cpp

--- Arrays/max_consecutive_ones_iii.cpp
+++ Arrays/max_consecutive_ones_iii.cpp
@@ -15,10 +15,14 @@ Bolded numbers were flipped from 0 to 1. The longest subarray is underlined.
 */
 
     int longestOnes(vector<int>& nums, int k) {
+        //i is the left of the sliding window
         int i=0;
+        //j is the right of the sliding window
         int j=0;
         while (j<nums.size()) {
+            //If we encounter a 0, while increasing our window, i.e. j -> decrease k
             if (nums[j] == 0) k--;
+            //If k becomes 0, then we need to move left of our window i.e. i. While decreasing window size, if nums[i] == 0, increase k again because we are throwing that 0 out of the window.
             if (k<0) {
                 if (nums[i] == 0) {
                     k++;
