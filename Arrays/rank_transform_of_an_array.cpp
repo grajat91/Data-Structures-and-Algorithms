5221b0986ff4d8e3de3ff600cb61acadb1c13fdc
grajat91 <72464777+grajat91@users.noreply.github.com>
2022-07-21 17:28:46 -0700

    Create rank_transform_of_an_array.cpp

---
+++ Arrays/rank_transform_of_an_array.cpp
@@ -0,0 +1,42 @@
+/*Given an array of integers arr, replace each element with its rank.
+
+The rank represents how large the element is. The rank has the following rules:
+
+Rank is an integer starting from 1.
+The larger the element, the larger the rank. If two elements are equal, their rank must be the same.
+Rank should be as small as possible.
+ 
+
+Example 1:
+
+Input: arr = [40,10,20,30]
+Output: [4,1,2,3]
+Explanation: 40 is the largest element. 10 is the smallest. 20 is the second smallest. 30 is the third smallest.
+Example 2:
+
+Input: arr = [100,100,100]
+Output: [1,1,1]
+Explanation: Same elements share the same rank.
+Example 3:
+
+Input: arr = [37,12,28,9,100,56,80,5,12]
+Output: [5,3,4,2,8,6,7,1,3]*/
+
+    vector<int> arrayRankTransform(vector<int>& arr) {
+        vector<int> copy_arr(arr);
+        sort(copy_arr.begin(),copy_arr.end());
+        unordered_map<int,int> rank;
+        int count=1;
+        for (int i=0;i<copy_arr.size();i++) {
+            if (rank.find(copy_arr[i]) == rank.end()) {
+                rank[copy_arr[i]] = count;
+                count++;
+            }
+        }
+        
+        for (int i=0;i<arr.size();i++) {
+            copy_arr[i] = rank[arr[i]];
+        }
+        
+        return copy_arr;
+    }
