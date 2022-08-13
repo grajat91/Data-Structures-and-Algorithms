11b967e133a1fc0eb73114e394d6a4e34ba1370a
grajat91 <72464777+grajat91@users.noreply.github.com>
2022-08-12 20:53:17 -0700

    Update check_completeness_of_binary_tree.cpp

--- binary_trees/check_completeness_of_binary_tree.cpp
+++ binary_trees/check_completeness_of_binary_tree.cpp
@@ -18,7 +18,7 @@ Constraints:
 
 The number of nodes in the tree is in the range [1, 100].
 1 <= Node.val <= 1000*/
-
+//BFS based solution
     bool isCompleteTree(TreeNode* root) {
         if (!root) return true;
         queue<TreeNode*> bfsq;
@@ -35,4 +35,19 @@ The number of nodes in the tree is in the range [1, 100].
             }
         }
         return true;
+    }
+
+//DFS based solution
+    int isCompleteTree_util(TreeNode* root) {
+        if (root == nullptr) return 0;
+        int l = isCompleteTree_util(root->left);
+        int r = isCompleteTree_util(root->right);
+        if ((l&(l+1)) == 0 && (l/2<=r && r<=l)) return l+r+1;
+        if ((r&(r+1)) == 0 && (r<=l && l<=(2*r+1))) return l+r+1;
+        //if ((l & (l + 1)) == 0 && l / 2 <= r && r <= l) return l+r+1;
+        //if ((r & (r + 1)) == 0 && r <= l && l <= r * 2 + 1) return l+r+1;
+        return -1;
+    }
+    bool isCompleteTree(TreeNode* root) {
+        return isCompleteTree_util(root)>=0;
     }
