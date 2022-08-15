51f6e72c56e78005b5a7910c76e8f6c185e78b66
grajat91 <72464777+grajat91@users.noreply.github.com>
2022-08-14 21:48:17 -0700

    Update K_closest_points_to_origin.cpp

--- priority_queue/K_closest_points_to_origin.cpp
+++ priority_queue/K_closest_points_to_origin.cpp
@@ -11,6 +11,7 @@ The distance between (-2, 2) and the origin is sqrt(8).
 Since sqrt(8) < sqrt(10), (-2, 2) is closer to the origin.
 We only want the closest k = 1 points from the origin, so the answer is just [[-2,2]].*/
 
+// Priority Queue Solution
     vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
         vector<vector<int>> result;
         priority_queue<pair<float,int>> pq;
@@ -31,4 +32,32 @@ We only want the closest k = 1 points from the origin, so the answer is just [[-
         }
         
         return result;
+    }
+
+//Quick sort partition function
+    bool operator_compare (const vector<int> &left, const vector<int> &right) {
+        return (left[0]*left[0])+(left[1]*left[1]) <= (right[0]*right[0])+(right[1]*right[1]);
+    }
+    
+
+    void quickselect_partition(vector<vector<int>>& points, int k, int left, int right) {
+        vector<int> pivot = points[right];
+        int pindex = left;
+        for (int i=left;i<right;i++) {
+            if (operator_compare(points[i],pivot)) {
+                swap(points[i],points[pindex]);
+                pindex++;
+            }
+        }
+        swap(points[right],points[pindex]);
+        if (pindex == k) return;
+        else if (pindex > k)  quickselect_partition(points,k,left,pindex-1);
+        else quickselect_partition(points,k, pindex+1, right);
+    }
+    vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
+        vector<vector<int>> result;
+        int n = points.size();
+        if (k == n) return points;
+        quickselect_partition(points,k,0,n-1);
+        return vector<vector<int>>(points.begin(), points.begin() + k);
     }
