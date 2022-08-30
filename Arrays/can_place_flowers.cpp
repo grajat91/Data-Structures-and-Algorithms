5201f8e3979b894a1a7940f8cf1970690dd528c2
grajat91 <72464777+grajat91@users.noreply.github.com>
2022-08-29 18:03:22 -0700

    Create can_place_flowers.cpp

---
+++ Arrays/can_place_flowers.cpp
@@ -0,0 +1,42 @@
+/*You have a long flowerbed in which some of the plots are planted, and some are not. However, flowers cannot be planted in adjacent plots.
+
+Given an integer array flowerbed containing 0's and 1's, where 0 means empty and 1 means not empty, and an integer n, return if n new flowers can be planted in the flowerbed without violating the no-adjacent-flowers rule.
+
+Example 1:
+
+Input: flowerbed = [1,0,0,0,1], n = 1
+Output: true
+Example 2:
+
+Input: flowerbed = [1,0,0,0,1], n = 2
+Output: false*/
+
+    bool canPlaceFlowers(vector<int>& nums, int n) {
+        int size = nums.size();
+        if (size == 1) {
+            if (nums[0] == 0 && n<=1) return true;
+            if (nums[0] == 1 && n==0) return true;
+            else return false;
+        } 
+        for (int i=0;i<size;i++) {
+            if (nums[i] == 0) {
+                if (i==0) {
+                    if (nums[1] == 0) {
+                        nums[0] = 1;
+                        n--;
+                    }    
+                } else if (i == size-1) {
+                    if (nums[size-2] == 0) {
+                        //nums[n-2] = 1;
+                        n--;
+                    }
+                } else {
+                    if (nums[i-1] == 0 && nums[i+1] == 0) {
+                        nums[i] = 1;
+                        n--;
+                    }
+                }
+            }
+        }
+        return (n<=0?true:false);
+    }
