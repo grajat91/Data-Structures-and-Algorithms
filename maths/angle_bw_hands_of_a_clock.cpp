80b20c272e0fe53b41e23ee02468c34f32292b5d
grajat91 <72464777+grajat91@users.noreply.github.com>
2022-10-18 08:54:44 -0700

    Update angle_bw_hands_of_a_clock.cpp

--- maths/angle_bw_hands_of_a_clock.cpp
+++ maths/angle_bw_hands_of_a_clock.cpp
@@ -21,6 +21,19 @@ Constraints:
 */
 
     double angleClock(int hour, int minutes) {
+     /*
+     Hour Hand
+     In 12 hours Hour hand complete whole circle and cover 360°
+     So, 1 hour = 360° / 12 = 30°
+
+     Since 1 hours = 30°
+     In 1 minute, hours hand rotate -> 30° / 60 = 0.5°
+     So total angle because of minutes by hour hand is minutes/60 * 30 or minutes * 0.5
+ 
+     Minute Hand
+     In 60 minutes Minute Hand completes whole circle and cover 360°.
+     So, 1 minute -> 360° / 60 = 6°
+     */
         double hour_angle = (hour%12)*30 + ((double)minutes*0.5) ;
         double minute_angle = 6*minutes;
         double diff = abs(minute_angle - hour_angle);
